e2ad1fdd94f7182f3ba6b426f3dca9ba18abf693
Alis Nikolaeva <2991768@gmail.com>
2024-06-26 18:27:36 -0400

    launch settings

--- src/MoneyControl.Server/Program.cs
+++ src/MoneyControl.Server/Program.cs
@@ -6,28 +6,34 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
+        builder.Configuration.AddEnvironmentVariables();
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddRazorPages();
+        builder.Services.AddHttpContextAccessor();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
         }
 
         app.UseHttpsRedirection();
 
-        app.UseAuthorization();
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
 
+        app.UseRouting();
+        app.UseCors("AllowAll");
 
+        app.MapRazorPages();
         app.MapControllers();
+        app.MapFallbackToFile("index.html");
 
         app.Run();
     }
